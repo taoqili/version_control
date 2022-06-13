8159e4b1e2d037c5d59ed0cbe57b94d2b0ba3022
陶启立 <taoqili@boulderaitech.com>
2022-06-13 09:10:20 +0800

    init

--- services/addVersion.js
+++ services/addVersion.js
@@ -1,16 +1,32 @@
 const path = require('path')
 const utils = require('../utils')
 module.exports = (req, res) => {
+  // TODO 此处需要部署本应用时配置
   const rootDir = path.resolve(__dirname, '../www')
-  const { project, version } = req.query || {}
+
+  const { project, version, repository, branch } = req.body || {}
   res.setHeader('Content-Type', 'text/html; charset=utf-8');
+  debugger
   if (!project || !version) {
-    res.send('缺失必要的参数，请检查后重试！')
+    res.send({
+      code: 1,
+      msg: '缺失必要的参数，请检查后重试！',
+    })
     return
   }
   try {
-    utils.addVersion(rootDir, project, version, res)
+    utils.addVersion({
+      rootDir,
+      res,
+      repository,
+      branch,
+      project,
+      version
+    })
   } catch (e) {
-    res.send('发布失败，请稍后重试！')
+    res.send({
+      code: 2,
+      msg: '发布失败，请稍后重试！',
+    })
   }
 }
--- utils/index.js
+++ utils/index.js
@@ -1,42 +1,43 @@
 const path = require('path')
 const shelljs = require('shelljs')
 module.exports = {
-  addVersion(rootDir, project, version, res) {
+  addVersion({repository, branch, rootDir, project, version, res}) {
     const targetDir = path.resolve(rootDir, project, version)
-    const origin = shelljs.exec('git remote -v')
-    const branchName = shelljs.exec('git rev-parse --abbrev-ref HEAD')
-    debugger
     const tmpDir = `./tmp/${project}`
     if (!shelljs.which('git')) {
-      res.send('未安装GIT')
+      res.send({
+        code: 3,
+        msg: '未安装GIT'
+      })
       return
     }
-    const { code } = shelljs.exec(`git clone https://github.com/taoqili/111.git ${tmpDir}`)
+    const { code } = shelljs.exec(`git clone ${repository} ${tmpDir}`)
     if (code !== 0) {
-      res.send('克隆仓库失败，请重试')
+      res.send({code: 4, msg: '克隆仓库失败，请重试'})
       return
     }
-    shelljs.echo('clone end, start to install npm')
+    shelljs.echo('clone end')
     shelljs.cd(tmpDir)
+    shelljs.exec(`git checkout ${branch}`)
     const { code: installCode } = shelljs.exec(`cnpm i`)
     if (installCode !== 0) {
-      res.send('依赖安装失败，请重试')
+      res.send({code: 6, msg: '依赖安装失败，请重试'})
       return
     }
     shelljs.echo('npm install end, start to build')
     const { code: buildCode } = shelljs.exec('npm run build')
     if (buildCode !== 0) {
-      res.send('构建失败，请重试')
+      res.send({code: 8, msg: '构建失败，请重试'})
       return
     }
     shelljs.echo('build end')
     shelljs.rm('-rf', targetDir)
     shelljs.echo('rm targetDir end')
-    shelljs.cp('-R', 'dist', targetDir)
+    shelljs.cp('-R', project, targetDir)
     shelljs.echo('copy end', targetDir)
     shelljs.cd('../../')
     shelljs.rm('-rf',tmpDir)
     shelljs.echo('rm tmp end', tmpDir)
-    res.send(`分支${branchName}发布成功!`)
+    res.send({code: 0, msg: `分支${branch}发布成功!`})
   }
 }
